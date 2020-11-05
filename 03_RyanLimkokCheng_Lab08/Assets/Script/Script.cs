d8c678d3c37a52b4116bd6c557b99c53540e35eb
SumanaiEugen <ryanlim943@gmail.com>
2020-11-05 13:56:13 +0800

    Lab8

---
+++ 03_RyanLimkokCheng_Lab08/Assets/Script/Script.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Script : MonoBehaviour
+{
+    float counter = 0;
+    public GameObject scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.GetComponent<Text>().text = ("Start Function");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            scoreText.GetComponent<Text>().text = ("Spacebar:" + counter);
+            print("Space Key pressed");
+            counter++;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            print("Space Key Released");
+        }
+
+    }
+}
